d047108ec46be4a72f4b49cb40612300a6fee1c3
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-03-20 20:10:31 -0400

    adds User model

---
+++ models/User.js
@@ -0,0 +1,73 @@
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+const sequelize = require("../config/connection");
+
+// create User model (table in our database)
+class User extends Model {
+	// password validation
+	checkPassword(loginPw) {
+		return bcrypt.compareSync(loginPw, this.password);
+	}
+}
+
+// specify what is contained within our user table
+User.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncremet: true,
+		},
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		email: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true,
+			validate: {
+				isEmail: true,
+			},
+		},
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: [8],
+			},
+		},
+	},
+	{
+		hooks: {
+			// password hashing before create
+			beforeCreate: async (newUserData) => {
+				newUserData.password = await bcrypt.hash(newUserData.password, 10);
+				return newUserData;
+			},
+			// password hashing before update
+			beforeUpdate: async (newUserData) => {
+				newUserData.password = await bcrypt.hash(newUserData.password, 10);
+				return newUserData;
+			},
+			// uniformity of email data before create
+			beforeCreate: async (newUserData) => {
+				newUserData.email = await newUserData.email.toLowerCase();
+				return newUserData;
+			},
+			// uniformity of email data before create
+			beforeUpdate: async (updatedUserData) => {
+				updatedUserData.email = await updatedUserData.email.toLowerCase();
+				return updatedUserData;
+			},
+		},
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: "user",
+	}
+);
+
+module.exports = User;
---
+++ models/index.js
@@ -0,0 +1,5 @@
+const User = require("./User");
+const BlogPost = require("./Blogpost");
+const Comment = require("./Comment");
+
+module.exports = { User, BlogPost, Comment };
