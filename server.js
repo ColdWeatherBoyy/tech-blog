c64ebf4d5161458392704045dc65951ea5d50679
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-03-21 10:33:38 -0400

    fixes styling and adds login route

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// the dependencies
 const express = require("express");
 const session = require("express-session");
 const path = require("path");
@@ -7,12 +8,14 @@ const helpers = require("./utils/helpers");
 const hbs = exphbs.create({ helpers });
 require("dotenv").config();
 
+// sequelize
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// sets up session constant
 const sess = {
 	secret: process.env.SESSION_SECRET,
 	cookie: { maxAge: 360000 },
@@ -23,17 +26,22 @@ const sess = {
 	}),
 };
 
+// middleware for sessions
 app.use(session(sess));
 
+// handlebars turned on
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
+// middleware for parsing and public folder
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
+// middleware for routes
 app.use("/", routes);
 
+// initialize sequelize and express listener
 sequelize.sync({ force: false }).then(() => {
 	app.listen(PORT, () => console.log("Now listening"));
 });
